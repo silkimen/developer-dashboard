20ea135b50793610e2ebd9adae3056388456fb2a
Sefa Ilkimen <sefa.ilkimen@googlemail.com>
2015-11-29 19:45:30 +0100

    using angular-ui-router; added empty views and controllers

--- app/js/ngapp.js
+++ app/js/ngapp.js
@@ -1,6 +1,42 @@
 /* global angular */
 
-var app = angular.module('developerDashboard', ['winjs']);
+var app = angular.module('developerDashboard', ['winjs', 'ui.router']);
+
+/* using angular-ui-router for routing */
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'HomeCtrl'
+			})
+			.state('tools', {
+				url: '/tools',
+				templateUrl: '/links.html',
+				controller: 'LinksCtrl'
+			})
+			.state('shared', {
+				url: '/shared',
+				templateUrl: '/links.html',
+				controller: 'LinksCtrl'
+			})
+			.state('private', {
+				url: '/private',
+				templateUrl: '/links.html',
+				controller: 'LinksCtrl'
+			})
+			.state('settings', {
+				url: '/settings',
+				templateUrl: '/settings.html',
+				controller: 'SettingsCtrl'
+			});
+
+		$urlRouterProvider.otherwise('home');
+	}
+]);
 
 app.factory('menu', [function () {
 		var menu = {};
@@ -39,5 +75,29 @@ app.controller('MainCtrl', [
 		$scope.splitViewInstance = document.getElementById('splitView');
 		$scope.greeter = 'Hello, world!';
 		$scope.menu = menu.items;
+	}
+]);
+
+app.controller('HomeCtrl', [
+	'$scope',
+	function ($scope) {
+		$scope.title='Developer Dashboard';
+		// just an empty controller, waiting to be filled with magic stuff
+	}
+]);
+
+app.controller('LinksCtrl', [
+	'$scope',
+	function ($scope) {
+		$scope.title='Links';
+		// just an empty controller, waiting to be filled with magic stuff
+	}
+]);
+
+app.controller('SettingsCtrl', [
+	'$scope',
+	function ($scope) {
+		$scope.title='Settings';
+		// just an empty controller, waiting to be filled with magic stuff
 	}
 ]);
