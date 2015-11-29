66111070992ea6505de316b1a612b031dacf4e7d
Sefa Ilkimen <sefa.ilkimen@googlemail.com>
2015-11-29 19:06:36 +0100

    added navigation menu items

--- app/js/ngapp.js
+++ app/js/ngapp.js
@@ -2,10 +2,42 @@
 
 var app = angular.module('developerDashboard', ['winjs']);
 
+app.factory('menu', [function () {
+		var menu = {};
+
+		// create some mockup menu items
+		menu.items = [
+		{
+			'label': 'Home',
+			'icon': 'home',
+			'target': 'home'
+		}, {
+			'label': 'Development Tools',
+			'icon': 'outlinestar',
+			'target': 'tools'
+		}, {
+			'label': 'Shared Bookmarks',
+			'icon': 'people',
+			'target': 'shared'
+		}, {
+			'label': 'Private Bookmarks',
+			'icon': 'contact',
+			'target': 'private'
+		}, {
+			'label': 'Settings',
+			'icon': 'settings',
+			'target': 'settings'
+		}];
+
+		return menu;
+}]);
+
 app.controller('MainCtrl', [
 	'$scope',
-	function ($scope) {
+	'menu',
+	function ($scope, menu) {
 		$scope.splitViewInstance = document.getElementById('splitView');
 		$scope.greeter = 'Hello, world!';
+		$scope.menu = menu.items;
 	}
 ]);
